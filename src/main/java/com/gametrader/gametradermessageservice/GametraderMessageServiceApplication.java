f27238ad19f41154f3047f206d68172c5fe522ad
ptrrk1337 <mymyry1999@gmail.com>
2022-03-07 19:04:55 +0100

    GAMETRADER-41 Initialized message service

---
+++ src/main/java/com/gametrader/gametradermessageservice/GametraderMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gametrader.gametradermessageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametraderMessageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GametraderMessageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gametrader/gametradermessageservice/GametraderMessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gametrader.gametradermessageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametraderMessageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
