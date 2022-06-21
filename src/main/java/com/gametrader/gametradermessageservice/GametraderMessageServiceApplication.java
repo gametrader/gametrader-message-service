eb5c458130f7d5e458ab27e980e02d7358fc592a
nicklastrange <mikolaj.cekut@gmail.com>
2022-06-21 19:11:49 +0200

    GAMETRADER-86 spring cloud configuration

--- src/main/java/com/gametrader/gametradermessageservice/GametraderMessageServiceApplication.java
+++ src/main/java/com/gametrader/gametradermessageservice/GametraderMessageServiceApplication.java
@@ -2,8 +2,10 @@ package com.gametrader.gametradermessageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GametraderMessageServiceApplication {
 
     public static void main(String[] args) {
